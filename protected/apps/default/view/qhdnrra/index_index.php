f1fc7f730698ad33235b9c2de59293fdd9ee51e0
zhangxuanru <1598484330@qq.com>
2018-05-16 23:29:02 +0800

    page index

--- protected/apps/default/view/qhdnrra/index_index.php
+++ protected/apps/default/view/qhdnrra/index_index.php
@@ -4,7 +4,7 @@
             <div id=banner class=top-banner>
             {link:banner}<!--全部链接使用link:all-->
              {if $link['picpath']}  
-                 <a style="BACKGROUND: url("[link:picpath]") no-repeat center top; HEIGHT: 360px;min-width:1200px; CURSOR: pointer" href="[link:url]" target="_blank"></a>
+                 <a style="BACKGROUND: url([link:picpath]) no-repeat center top; HEIGHT: 360px;min-width:1200px; CURSOR: pointer" href="[link:url]" target="_blank"></a>
              {/if} 
              {/link} 
             </div>
@@ -15,81 +15,111 @@
     </tr>
 </table>
 
+ 
 
-<table width="1200" border="0" align="center" cellpadding="0" cellspacing="0" style="margin-top:10px; margin: 0 auto">
-    <tr>
-        <td width="300" height="80" valign="top" bgcolor="#FFFFFF" style="padding-top:15px;">
-            <div class="yuevents fl clearfix">
-                <div class="events_title"><b>通知公告</b>
-                    <span>Events</span><a class="more fr" href="http://news.buaa.edu.cn/xsjwhhd_new.htm" target="_blank">更多</a>
-                </div>
-                <div>
-                    <dl class="eventsDl eventsDlA">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43549.htm" title="【北航音乐厅预告】北京交响乐团“坚定文化自信•弘扬正能量”2018年进校园北航专场音乐会" target="_blank">【北航音乐厅预告】北京交响乐团“坚定文化自信•弘扬正能量”2018年进校园北航专场音乐会</a></dt>
-                        <dd>2018.05.08</dd>
-                    </dl>
-                </div>
-            </div>
+<div class="mainL" style="width: 1200px;margin-left:auto">
+ <div class="eventVoice clearfix">
+                        <div class="events fl clearfix">
+                            <div class="events_title"><b>活动资讯</b><span>Events</span><a class="more fr" href="http://news.buaa.edu.cn/xsjwhhd_new.htm" target="_blank">更多</a></div>    
+                            <!--#begineditable name="活动资讯标题列表" viewid="50730"--><div class="fl">
+<dl class="eventsDl eventsDlA">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43708.htm" title="【预告】北航大讲堂|空间与环境学院第二届创新论坛暨致真书院高端论坛系列讲座" target="_blank">【预告】北航大讲堂|空间与环境学院第二届创新论坛暨致真书院高端论坛系列讲座</a></dt>
+            <dd>2018.05.16</dd>
+        </dl>
 
-        </td>
-        <td width="450" valign="top" bgcolor="#FFFFFF">
-            <div class="events fl clearfix">
-                <div class="events_title"><b>活动资讯</b><span>Events</span><a class="more fr" href="http://news.buaa.edu.cn/xsjwhhd_new.htm" target="_blank">更多</a></div>
-                <!--#begineditable name="活动资讯标题列表" viewid="50730"-->
-                <div class="fl">
-                    <dl class="eventsDl eventsDlA">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43549.htm" title="【北航音乐厅预告】北京交响乐团“坚定文化自信•弘扬正能量”2018年进校园北航专场音乐会" target="_blank">【北航音乐厅预告】北京交响乐团“坚定文化自信•弘扬正能量”2018年进校园北航专场音乐会</a></dt>
-                        <dd>2018.05.08</dd>
-                    </dl>
+<dl class="eventsDl eventsDlB">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43706.htm" title="【北航音乐厅预告】开心麻花话剧《婿事待发》" target="_blank">【北航音乐厅预告】开心麻花话剧《婿事待发》</a></dt>
+            <dd>2018.05.16</dd>
+        </dl>
 
-                    <dl class="eventsDl eventsDlB">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43533.htm" title="【预告】2018届本科生“毕业嘉年华”活动" target="_blank">【预告】2018届本科生“毕业嘉年华”活动</a></dt>
-                        <dd>2018.05.08</dd>
-                    </dl>
+<dl class="eventsDl eventsDlA">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43661.htm" title="关于开展我校青年教师与校友企业产学研交流活动的通知" target="_blank">关于开展我校青年教师与校友企业产学研交流活动的通知</a></dt>
+            <dd>2018.05.15</dd>
+        </dl>
 
-                    <dl class="eventsDl eventsDlA">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43521.htm" title="【预告】北航高研论坛•非物质文化遗产系列讲座 | 覃代伦：锦说中华——中国民族织锦艺术审美史纲" target="_blank">【预告】北航高研论坛•非物质文化遗产系列讲座 | 覃代伦：锦说中华——中国民族织锦艺术审美史纲</a></dt>
-                        <dd>2018.05.08</dd>
-                    </dl>
-
-                    <dl class="eventsDl eventsDlB">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43471.htm" title="研究生人文与科学素养系列讲座预告（5.7-5.13）" target="_blank">研究生人文与科学素养系列讲座预告（5.7-5.13）</a></dt>
-                        <dd>2018.05.06</dd>
-                    </dl>
-
-                </div>
-
-                <div class="fr">
+<dl class="eventsDl eventsDlB">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43631.htm" title="【北航音乐厅预告】第十八届“相约北京”艺术节——歌舞多彩巴尔干——塞尔维亚贝尔格莱德大学民族歌舞团和民族乐团" target="_blank">【北航音乐厅预告】第十八届“相约北京”艺术节——歌舞多彩巴尔干——塞尔维亚贝尔格莱德大学民族歌舞团和民族乐团</a></dt>
+            <dd>2018.05.14</dd>
+        </dl>
 
 
 
 
-                    <dl class="eventsDl eventsDlB">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43377.htm" title="【北航音乐厅预告】波兰公平竞争组合肢体舞蹈默剧《严肃喜剧》" target="_blank">【北航音乐厅预告】波兰公平竞争组合肢体舞蹈默剧《严肃喜剧》</a></dt>
-                        <dd>2018.05.02</dd>
-                    </dl>
 
-                    <dl class="eventsDl eventsDlA">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43366.htm" title="学术讲座预告：【北航自动化大讲堂 第2期】 澳大利亚皇家墨尔本理工大学余星火教授特邀报告" target="_blank">学术讲座预告：【北航自动化大讲堂 第2期】 澳大利亚皇家墨尔本理工大学余星火教授特邀报告</a></dt>
-                        <dd>2018.04.28</dd>
-                    </dl>
+    </div>
+    
+    <div class="fr">
 
-                    <dl class="eventsDl eventsDlB">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43307.htm" title="【北航思源讲堂】首讲预告：春天不一样的美食养生——吃“春儿”" target="_blank">【北航思源讲堂】首讲预告：春天不一样的美食养生——吃“春儿”</a></dt>
-                        <dd>2018.04.26</dd>
-                    </dl>
 
-                    <dl class="eventsDl eventsDlA">
-                        <dt><a href="http://news.buaa.edu.cn/info/1011/43293.htm" title="空间与环境学院第二届创新论坛第三讲预告：从北航到南门二" target="_blank">空间与环境学院第二届创新论坛第三讲预告：从北航到南门二</a></dt>
-                        <dd>2018.04.25</dd>
-                    </dl>
 
-                </div><script>_showDynClickBatch(['dynclicks_u9_3016','dynclicks_u9_3014','dynclicks_u9_3012','dynclicks_u9_3006','dynclicks_u9_2982','dynclicks_u9_2980','dynclicks_u9_2972','dynclicks_u9_2971'],[3016,3014,3012,3006,2982,2980,2972,2971],"wbnews", 1405884961)</script>
-                <!--#endeditable-->
-            </div>
 
-    </tr>
-</table>
+<dl class="eventsDl eventsDlB">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43624.htm" title="研究生人文与科学素养系列讲座预告（5.14-5.20）" target="_blank">研究生人文与科学素养系列讲座预告（5.14-5.20）</a></dt>
+            <dd>2018.05.13</dd>
+        </dl>
+
+<dl class="eventsDl eventsDlA">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43549.htm" title="【北航音乐厅预告】北京交响乐团“坚定文化自信•弘扬正能量”2018年进校园北航专场音乐会" target="_blank">【北航音乐厅预告】北京交响乐团“坚定文化自信•弘扬正能量”2018年进校园北航专场音乐会</a></dt>
+            <dd>2018.05.08</dd>
+        </dl>
+
+<dl class="eventsDl eventsDlB">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43533.htm" title="【预告】2018届本科生“毕业嘉年华”活动" target="_blank">【预告】2018届本科生“毕业嘉年华”活动</a></dt>
+            <dd>2018.05.08</dd>
+        </dl>
+
+<dl class="eventsDl eventsDlA">
+            <dt><a href="http://news.buaa.edu.cn/info/1011/43521.htm" title="【预告】北航高研论坛•非物质文化遗产系列讲座 | 覃代伦：锦说中华——中国民族织锦艺术审美史纲" target="_blank">【预告】北航高研论坛•非物质文化遗产系列讲座 | 覃代伦：锦说中华——中国民族织锦艺术审美史纲</a></dt>
+            <dd>2018.05.08</dd>
+        </dl>
+
+ 
+                        </div>
+                        <div class="voice fr clearfix" style="margin-left: 60px;">
+                            <div class="voice_title"><b>通知公告</b><span>Notice</span><a class="more fr" href="http://news.buaa.edu.cn/xxgg_new.htm" target="_blank">更多</a></div>  
+                            <!--#begineditable name="通知公告标题列表" viewid="50732"--><dl class="clearfix" style="margin-top: 0">
+                                <dt>04.12</dt>
+                                <dd><p><a href="http://news.buaa.edu.cn/info/1010/43033.htm" title="北京航空航天大学关于公开遴选部分学院院长的公告" target="_blank">北京航空航天大学关于公开遴选部分学院院长的公告</a></p></dd>
+                            </dl>
+<dl class="clearfix">
+                                <dt>05.16</dt>
+                                <dd><p><a href="http://news.buaa.edu.cn/info/1010/43710.htm" title="2018年大学生征兵入伍工作通知暨征兵优待政策简介" target="_blank">2018年大学生征兵入伍工作通知暨征兵优待政策简介</a></p></dd>
+                            </dl>
+<dl class="clearfix">
+                                <dt>05.15</dt>
+                                <dd><p><a href="http://news.buaa.edu.cn/info/1010/43678.htm" title="关于举办2018年北京航空航天大学校园开放日暨高招咨询活动的通知" target="_blank">关于举办2018年北京航空航天大学校园开放日暨高招咨询活动的通知</a></p></dd>
+                            </dl>
+<dl class="clearfix">
+                                <dt>05.09</dt>
+                                <dd><p><a href="http://news.buaa.edu.cn/info/1010/43569.htm" title="关于办理2018年北航机动车停车证的通告" target="_blank">关于办理2018年北航机动车停车证的通告</a></p></dd>
+                            </dl>
+<dl class="clearfix">
+                                <dt>05.08</dt>
+                                <dd><p><a href="http://news.buaa.edu.cn/info/1010/43548.htm" title="2018年莫斯科航空学院暑期学校通知" target="_blank">2018年莫斯科航空学院暑期学校通知</a></p></dd>
+                            </dl>
+<dl class="clearfix">
+                                <dt>05.07</dt>
+                                <dd><p><a href="http://news.buaa.edu.cn/info/1010/43507.htm" title="关于申报2018年北航“学院国际评估支持计划”的通知" target="_blank">关于申报2018年北航“学院国际评估支持计划”的通知</a></p></dd>
+                            </dl>
+<dl class="clearfix">
+                                <dt>05.03</dt>
+                                <dd><p><a href="http://news.buaa.edu.cn/info/1010/43409.htm" title="关于校园西路、科技园路一侧行车道临时封闭的通知" target="_blank">关于校园西路、科技园路一侧行车道临时封闭的通知</a></p></dd>
+                            </dl>
+ 
+                        </div>
+                    </div>
+
+                    </div>
+
+
+
+
+
+
+
+
+
+
 
 <table width="1200" border="0" align="center" cellpadding="0" cellspacing="0" style="margin-top:10px; margin-bottom:40px; margin:0 auto">
     <tr>
