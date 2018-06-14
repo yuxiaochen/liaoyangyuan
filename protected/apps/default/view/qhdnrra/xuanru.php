6fab9a6bdf8bcdeafdde8148cb1b666d480353da
yuxiaochen <274344972@qq.com>
2018-06-14 18:31:55 +0800

    xuanru

---
+++ protected/apps/default/view/qhdnrra/xuanru.php
@@ -0,0 +1,478 @@
+<?php
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>巴黎高科集团留学宣讲会预告-新闻网</title>
+<link rel="stylesheet" type="text/css" href="../../dfiles/12596/dfiles/12596/css/headerfooter.css" />
+<link rel="stylesheet" type="text/css" href="../../dfiles/12596/dfiles/12596/css/style.css" />
+<script type="text/javascript" src="../../dfiles/12596/dfiles/12596/js/js.js"></script>
+<!--Announced by Visual SiteBuilder 9-->
+<link rel="stylesheet" type="text/css" href="../../dfiles/12596/_sitegray/_sitegray_d.css" />
+<script language="javascript" src="../../dfiles/12596/_sitegray/_sitegray.js"></script>
+<!-- CustomerNO:77656262657232307e7847545254574503060004 -->
+<link rel="stylesheet" type="text/css" href="../../dfiles/12596/content.vsb.css" />
+<meta name="keywords" content="新闻网,综合新闻" />
+
+<!--Announced by Visual SiteBuilder 9-->
+<link rel="stylesheet" type="text/css" href="../../_sitegray/_sitegray_d.css" />
+<script language="javascript" src="../../_sitegray/_sitegray.js"></script>
+<!-- CustomerNO:77656262657232307e7847545254574503060004 -->
+<link rel="stylesheet" type="text/css" href="../../content.vsb.css" />
+<META Name="keywords" Content="新闻网,学术及文化活动" />
+<script type="text/javascript" src="../../system/resource/js/vsbscreen.min.js" id="_vsbscreen" devices="pc|pad"></script>
+<script type="text/javascript" src="../../system/resource/js/counter.js"></script>
+<script type="text/javascript">_jsq_(1011,'/content.jsp',44232,1405936382)</script>
+</head>
+
+<body>
+
+
+
+<div class="header auto">
+<div class="headerbox w1102">
+<div class="logo auto">
+
+
+
+
+<!--#begineditable name="LOGO" viewid="50857"-->
+
+<a href="../../index.htm"><img src="../../dfiles/12596/images/logo.jpg" /></a><!--#endeditable-->
+
+
+</div>
+<div class="search auto">
+
+
+<!--#begineditable name="全文检索" viewid="50866"--><script type="text/javascript">
+function _nl_ys_check(){
+
+    var keyword = document.getElementById('showkeycode50866').value;
+    if(keyword==null||keyword==""){
+        alert("请输入你要检索的内容！");
+        return false;
+    }
+    if(window.toFF==1)
+    {
+        document.getElementById("lucenenewssearchkeyword50866").value = Simplized(keyword );
+    }else
+    {
+        document.getElementById("lucenenewssearchkeyword50866").value = keyword;
+    }
+    var  base64 = new Base64();
+    document.getElementById("lucenenewssearchkeyword50866").value = base64.encode(document.getElementById("lucenenewssearchkeyword50866").value);
+    new VsbFormFunc().disableAutoEnable(document.getElementById("showkeycode50866"));
+    return true;
+}
+</script>
+<form action="../../ssjg.jsp?wbtreeid=1011" method="post" id="au1a" name="au1a" onsubmit="return _nl_ys_check()" style="display: inline">
+<input type="hidden" id="lucenenewssearchkeyword50866" name="lucenenewssearchkeyword" value=""><input type="hidden" id="_lucenesearchtype50866" name="_lucenesearchtype" value="1"><input type="hidden" id="searchScope50866" name="searchScope" value="0">
+
+<input name="showkeycode" id="showkeycode50866" class="searchtext">
+<input type="submit" class="searchsub" value="">
+</form>
+
+
+
+
+
+<script language="javascript" src="../../system/resource/js/base64.js"></script><script language="javascript" src="../../system/resource/js/formfunc.js"></script>
+<!--#endeditable-->
+
+
+
+</div>
+<div class="topnav auto">
+
+
+
+
+<!--#begineditable name="友情链接" viewid="50858"--><script language="javascript" src="../../system/resource/js/dynclicks.js"></script><script language="javascript" src="../../system/resource/js/openlink.js"></script><a href="http://www.buaa.edu.cn/">北航官网</a>
+<a href="http://oldnews.buaa.edu.cn/">旧版新闻网</a>
+<a href="http://ev.buaa.edu.cn/">ENGLISH</a>
+<a href="http://i.buaa.edu.cn/">IHome</a>
+
+
+
+
+<!--#endeditable-->
+
+
+
+
+
+</div>
+</div>
+</div>
+
+<div class="nav auto">
+<div class="navbox w1102">
+<!--#begineditable name="网站导航" viewid="50829"--><ul>
+<li><a href="../../index.htm">首页</a></li>
+<li><a href="../../zhxw_new.htm">综合新闻</a></li>
+<li><a href="../../ztxw_new.htm">专题新闻</a></li>
+<li><a href="../../bhrw.htm">北航人物</a></li>
+<li><a href="../../xyfc_new.htm">校园风采</a></li>
+<li><a href="../../kjzx_new.htm">科教在线</a></li>
+<li><a href="../../mtbh_new.htm">媒体北航</a></li>
+<li><a href="../../gybh_new.htm">光影北航</a></li>
+<li><a href="../../spxw1.htm">视频新闻</a></li>
+<li><a href="../../wyyd_new.htm">文艺园地</a></li>
+<li><a href="../../xxgg_new.htm">信息公告</a></li>
+<li><a href="../../xsjwhhd_new.htm" class="current">学术及文化活动</a></li>
+<li><a href="../../zxtg.jsp?urltype=tree.TreeTempUrl&wbtreeid=1012">网上投稿</a></li>
+</ul><!--#endeditable-->
+</div>
+</div>
+
+<div class="subnav auto">
+/<span><!--#begineditable name="当前栏目" viewid="50832"-->学术及文化活动<!--#endeditable--></span>
+</div>
+
+<div class="main w1102">
+
+<script language="javascript" src="../../dfiles/12596/_dwr/interface/newsvotedwr.js"></script><script language="javascript" src="../../dfiles/12596/_dwr/engine.js"></script><script language="javascript" src="../../dfiles/12596/system/resource/js/news/newscontent.js"></script><script type="text/javascript">NewsvoteDWR.getNewsLinkUrl(42574,'1323473998','vsb',newsSkip);function newsSkip(data){if(data != ""){window.location.href = data;}}</script><link href="../../dfiles/12596/system/resource/js/photoswipe/3.0.5.1/photoswipe.css" type="text/css" rel="stylesheet"><script language="javascript" src="../../dfiles/12596/system/resource/js/photoswipe/3.0.5.1/klass.min.js"></script><script language="javascript" src="../../dfiles/12596/system/resource/js/ajax.js"></script><form name="_newscontent_fromname">
+
+
+
+
+<!--#begineditable name="文章内容" viewid="50828"--><script language="javascript" src="../../_dwr/interface/NewsvoteDWR.js"></script><script language="javascript" src="../../_dwr/engine.js"></script><script language="javascript" src="../../system/resource/js/news/newscontent.js"></script><script type="text/javascript">NewsvoteDWR.getNewsLinkUrl(44232,'1405936382','vsb',newsSkip);function newsSkip(data){if(data != ""){window.location.href = data;}}</script><LINK href="../../system/resource/js/photoswipe/3.0.5.1/photoswipe.css" type="text/css" rel="stylesheet"><script language="javascript" src="../../system/resource/js/photoswipe/3.0.5.1/klass.min.js"></script><script language="javascript" src="../../system/resource/js/ajax.js"></script><div class="newsleft auto">
+<div class="newslefttit auto">
+<h1>巴黎高科集团留学宣讲会预告</h1>
+</div>
+<div class="newsleftcon auto">
+<div class="newsleftconmes auto">
+<p><span class="lf"></span><span class="ri">点击数:<script>_showDynClicks("wbnews", 1405936382, 44232)</script>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;加入时间:2018-06-11</span></p>
+</div>
+<div class="newsleftconbox auto" id="vsb_content">
+<div id="vsb_content"><p class="vsbcontent_start">6月13日（周三）下午，巴黎高科集团（ParisTech）驻华主任Sylvain FERRARI先生将访问我校，向我校学生全面介绍巴黎高科的留学机会。 </p>
+<p>有意赴该校攻读硕博项目的同学，欢迎踊跃参加！ </p>
+<p>【时间】2018年6月13日（星期三）15 :00-16 :00 </p>
+<p>【地点】学院路校区新主楼会议中心第二会议室 </p>
+<p>【主讲人】巴黎高科集团（ParisTech）驻华主任Sylvain FERRARI先生 </p>
+<p>【对象】理工类、经济类本科及硕士生 </p>
+<p><strong><span style="font-size: 14px">学校简介</span></strong></p>
+<p>巴黎高科（ParisTech）汇集了全法最具声望的10所工程师学校，每所学校都在不同的专业领域享誉盛名，其教学、科研范围覆盖了工程学、理学和经济学的所有学科，旨在培养杰出的研究型及管理型人才。</p>
+<p><strong><span style="font-size: 14px">巴黎高科成员学校</span></strong></p>
+<p>巴黎高科环境与生命科学工程学院(Agro ParisTech) </p>
+<p>巴黎高科国立高等工程技术学校(Arts et M&eacute;tiers ParisTech)</p>
+<p>巴黎高科国立化学学校(Chimie ParisTech)</p>
+<p>巴黎高科国立桥路学校(Ecole des Ponts ParisTech)</p>
+<p>巴黎高科国立统计与经济管理学校( ENSAE ParisTech)</p>
+<p>巴黎高科高等先进技术学校(ENSTA ParisTech)</p>
+<p>巴黎高科高等理工化工学校(ESPCI Paris)</p>
+<p><a href="http://baike.baidu.com/view/3978584.htm">高等光学学院</a>（Institut d'Optique Graduates School）</p>
+<p>巴黎高科国立巴黎高等矿业学校(MINES ParisTech)</p>
+<p>巴黎高科<a href="http://baike.baidu.com/view/4247753.htm">国立高等电信学校</a>(T&eacute;l&eacute;com ParisTech)</p>
+<p class="vsbcontent_img">
+ <img border="0" src="../../_mediafile/xwwx/2018/06/11/2e1w2iarr8.jpg" orisrc="/_mediafile/xwwx/2018/06/11/source/2e1w2iarr8.jpg" class="img_vsb_content"></p>
+<p><strong><span style="font-size: 14px">巴黎高科各校教学领域</span></strong></p>
+<p class="vsbcontent_end">应用数学–计算机科学–统计学–金融–物理学–光学科学–经济与社会科学–工程学-工业与生产工程–管理–材料科学与工程–机械工程–海洋工程–船建工程–电气、电子与光子工程–信息技术与电信–机器人–自动化–能源与核工程–化学–化工–地球科学–环境工程–生命科学–食品工程–森林工程–农业工程–计算生物学–生物工程和生物物理 </p>
+<p class="vsbcontent_end" style="text-align: right">编辑：宋超</p></div><div id="div_vote_id"></div>
+                </div>
+            </div>
+
+        </div><!--#endeditable--><!--leftOver-->
+
+        <div class="newsright auto">
+
+            <div class="newsright1 auto">
+                <a href="#" onclick="javascript:window.print()">打印</a>
+                <script type="text/javascript"><!--
+var SubjectLine=''+top.location.href;
+var BodyText='Address:'+top.location.href;
+var Message='<A CLASS=tomail HREF="mailto:?SUBJECT='+escape(SubjectLine)+'&BODY='+escape(BodyText)+'" OnMouseOver="status=\'\'; return true;" TITLE="">发送邮件<\/A>';
+var MessageIE='<A CLASS=tomail HREF="mailto:?SUBJECT='+(SubjectLine)+'&BODY='+(BodyText)+'" OnMouseOver="status=\'\'; return true;" TITLE="">发送邮件<\/A>';
+if(document.all) { document.write(MessageIE); }
+else { document.write(Message); }
+//--></script>
+            </div>
+
+            <div class="newsright2 auto">
+                <div class="newsright2tit auto">分享</div>
+                <div class="newsright2con auto">
+                    <div class="bdshare">
+                                                <!-- Baidu Button BEGIN -->
+                                                <span style="float:left"><a href="#"><img src="../../dfiles/12596/dfiles/12596/images/shareicon1.jpg" /></a></span><div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare"> <a class="bds_tsina"></a> <a class="bds_qzone"></a> <a class="bds_renren"></a> <a class="bds_tqq"></a> <a class="bds_douban"></a> <span class="bds_more"></span> </div>
+                                                <script type="text/javascript" id="bdshare_js" data="type=tools&amp;uid=6838818"></script>
+                                                <script type="text/javascript" id="bdshell_js"></script>
+                                                <script type="text/javascript">
+                        document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000)
+                        </script>
+                                                <!-- Baidu Button END -->
+                  </div>
+                </div>
+
+            </div>
+
+
+  
+
+
+
+</form>
+
+
+            <div class="newsright3 auto">
+                <div class="newsright3tit auto"><img src="../../dfiles/12596/dfiles/12596/images/showsidetit.jpg" /></div>
+                <div class="newsright3con auto">
+                    <!--#begineditable name="最近新闻" viewid="50830"--><ul>
+   <li>
+                                     <a href="44232.htm"><img src="../../dfiles/12596/images/sideimg.jpg" /></a>
+     
+
+                                               <p><a href="44232.htm" target="_blank" title="巴黎高科集团留学宣讲会预告">巴黎高科集团留学宣讲会预告</a></p>
+                        </li>
+<li>
+                   
+     
+
+                                               <p><a href="44230.htm" target="_blank" title="研究生人文与科学素养系列讲座预告（6.11-6.17）">研究生人文与科学素养系列讲座预告（6.11-6.17）</a></p>
+                        </li>
+<li>
+                   
+     
+
+                                               <p><a href="44216.htm" target="_blank" title="【北航音乐厅预告】 京剧行当艺术经典剧目展演《荀灌娘》《三岔口》《杜十娘》">【北航音乐厅预告】 京剧行当艺术经典剧目展演《荀灌娘》《三岔口》《杜十娘》</a></p>
+                        </li>
+<li>
+                   
+     
+
+                                               <p><a href="44204.htm" target="_blank" title="航空制造大讲堂——工业4.0系列讲座预告">航空制造大讲堂——工业4.0系列讲座预告</a></p>
+                        </li>
+</ul>
+<script>_showDynClickBatch(['dynclicks_u6_44232','dynclicks_u6_44230','dynclicks_u6_44216','dynclicks_u6_44204'],[44232,44230,44216,44204],"wbnews", 1405936382)</script>
+<!--#endeditable-->
+                </div>
+            </div>
+
+        </div><!--rightOver-->
+
+    </div><!--main1Over-->
+
+    <div class="showmain2 w1102">
+        <div class="showmain2tit w1102">更多新闻</div>
+        <!--#begineditable name="更多新闻" viewid="50831"--><div class="showmain2left2box w1102">
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44232.htm" target="_blank">            巴黎高科集团留学宣讲会预告
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44232" name="dynclicks_u7_44232"></span><br />加入时间:2018-06-11</span>
+                </div>
+            </div>
+
+
+       
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44230.htm" target="_blank">            研究生人文与科学素养系列讲座预告（6.11-6....
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44230" name="dynclicks_u7_44230"></span><br />加入时间:2018-06-11</span>
+                </div>
+            </div>
+
+
+       
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44216.htm" target="_blank">            【北航音乐厅预告】 京剧行当艺术经典剧目展...
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44216" name="dynclicks_u7_44216"></span><br />加入时间:2018-06-11</span>
+                </div>
+            </div>
+
+
+       
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44204.htm" target="_blank">            航空制造大讲堂——工业4.0系列讲座预告
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44204" name="dynclicks_u7_44204"></span><br />加入时间:2018-06-10</span>
+                </div>
+            </div>
+
+
+       
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44205.htm" target="_blank">            航空制造大讲堂讲座预告
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44205" name="dynclicks_u7_44205"></span><br />加入时间:2018-06-10</span>
+                </div>
+            </div>
+
+
+       
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44146.htm" target="_blank">            【北航音乐厅预告】 “古典与浪漫”2018国际...
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44146" name="dynclicks_u7_44146"></span><br />加入时间:2018-06-06</span>
+                </div>
+            </div>
+
+
+       
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44127.htm" target="_blank">            【通知】话剧《屈原》演出取消及退票通知
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44127" name="dynclicks_u7_44127"></span><br />加入时间:2018-06-06</span>
+                </div>
+            </div>
+
+
+       
+   <div class="main2left2box auto">
+                <div class="main2left2date auto">06
+月<br /><span>
+
+06
+</span></div>
+                <div class="main2left2img auto">
+<img src="" width="80" height="80" />
+
+</div>
+                <div class="main2left2text auto">
+                    <a href="44112.htm" target="_blank">            【北航音乐厅预告】“琳琅芳华”——中国声...
+    </a>
+                    <p></p>
+                    <span>点击数:<span id="dynclicks_u7_44112" name="dynclicks_u7_44112"></span><br />加入时间:2018-06-05</span>
+                </div>
+            </div>
+
+
+       
+</div><script>_showDynClickBatch(['dynclicks_u7_44232','dynclicks_u7_44230','dynclicks_u7_44216','dynclicks_u7_44204','dynclicks_u7_44205','dynclicks_u7_44146','dynclicks_u7_44127','dynclicks_u7_44112'],[44232,44230,44216,44204,44205,44146,44127,44112],"wbnews", 1405936382)</script>
+<!--#endeditable--><script>_showDynClickBatch(['dynclicks_u7_42574','dynclicks_u7_42654','dynclicks_u7_42593','dynclicks_u7_42592','dynclicks_u7_42621','dynclicks_u7_42607','dynclicks_u7_42613','dynclicks_u7_42590'],[42574,42654,42593,42592,42621,42607,42613,42590],"wbnews", 1323473998)</script>
+
+    </div>
+
+    <!--main2Over-->
+
+
+<div class="footertext auto">
+        <div class="copyright lf auto">
+         <!-- 版权内容请在本组件"内容配置-版权"处填写 -->
+<!--#begineditable name="版权" viewid="50860"--><!-- 版权内容请在本组件"内容配置-版权"处填写 -->
+版权所有&nbsp;2010&nbsp;北京航空航天大学&nbsp;&nbsp;&nbsp;&nbsp;京ICP备05004617&nbsp;&nbsp;&nbsp;&nbsp;文保网安备案号1101080018&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;地址：北京市海淀区学院路37号&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;邮编：100083&nbsp;&nbsp;电话：82317114
+<div style="display:none">
+<script>
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?f2dfd8a15ace140a5d7c5f1bbc89b6b4";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(hm, s);
+})();
+</script>
+</div><!--#endeditable-->
+
+
+ <br />
+     
+     
+               <!--#begineditable name="底部链接" viewid="50861"-->
+ <a href="#" title="" onclick="_addDynClicks(&#34;wburl&#34;, 1405936382, 3941)">网站地图</a>
+
+
+ <a href="#" title="" onclick="_addDynClicks(&#34;wburl&#34;, 1405936382, 3942)">联系我们</a>
+
+
+ <a href="#" title="" onclick="_addDynClicks(&#34;wburl&#34;, 1405936382, 3943)">版权隐私</a>
+
+
+ <a href="#" title="" onclick="_addDynClicks(&#34;wburl&#34;, 1405936382, 3944)">友情链接</a>
+
+<!--#endeditable-->
+
+
+        </div>
+
+<div style="display:none">
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?f2dfd8a15ace140a5d7c5f1bbc89b6b4";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+</div>
+    </div><script src="../../dfiles/12596/dfiles/12596/cms/web/d89b55b8e7ba7b4c6eb7565dc45864ba_writelog.jpg.js"></script>
+
+</body></html>
