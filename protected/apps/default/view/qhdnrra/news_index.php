fbf41e40d6702fe67a1b73cc715c805ff7821c9c
yuxiaochen <274344972@qq.com>
2018-07-03 23:58:12 +0800

    aa

--- protected/apps/default/view/qhdnrra/news_index.php
+++ protected/apps/default/view/qhdnrra/news_index.php
@@ -1,12 +1,8 @@
 <?php if(!defined('APP_NAME')) exit;?>
-<div class="layout listbanner bg-blue">
-    <div class="container"><h1 class="fadein-top">YXcms Demo</h1><p class="fadein-bottom">该模板采用前端框架式架构，兼容PC、平板和手机</p></div>
-</div>
-
 <div class="container margin-top">
   <div class="line-middle">
     <div class="xm9">
-      <ul class="bread bg-blue bg-inverse">
+      <ul class="bread bg-blue bg-inverse" style="background-image:url('/public/default/qhdnrra/images/qhdnrra/qhdnrra_in_bg.gif')">
          <li><a href="{url()}" class="icon-home"> 首页</a></li>
          {loop $daohang $vo}
               <li><a href="{$vo['url']}">{$vo['name']}</a></li>
@@ -17,7 +13,7 @@
              {loop $alist $vo}
              <li>
                  <div class="media media-x">
-                     {if $vo['picture'] && $vo['picture']!='NoPic.gif'}<a class="float-left" title="{$vo['title']}" href="{$vo['url']}" target="_blank"><img src="{$vo['picturepath']}" class="radius" alt="{$vo['title']}"></a>{/if}
+                     {if $vo['picture'] && $vo['picture']!='NoPic.gif'}<a class="float-left" title="{$vo['title']}" href="{$vo['url']}" target="_blank"><img src="{$vo['picturepath']}" class="radius" style="max-width:450px;max-height:250px" alt="{$vo['title']}"></a>{/if}
                      <div class="media-body height-big">
                          <strong class="float-left" style="color:{$vo['color']}">{$vo['title']}</strong>
                          <span class="text-gray float-right hidden-l">{date($vo['addtime'],Y-m-d H:m:i)}&nbsp;&nbsp;点击:{$vo['hits']}</span>
