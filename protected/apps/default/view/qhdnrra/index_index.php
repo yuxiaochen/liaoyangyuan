874006a77e3ab088f12d8762939d66143f490a6d
yuxiaochen <274344972@qq.com>
2018-07-08 18:12:20 +0800

    update index theme

--- protected/apps/default/view/qhdnrra/index_index.php
+++ protected/apps/default/view/qhdnrra/index_index.php
@@ -1,3 +1,8 @@
+<<style type="text/css">
+   .newsR{
+    width: 380px;
+   }
+</style>
 <div class="layout  fadein-left">
     <div class="container">
         <div class="banner" data-pointer="1" data-interval="6" data-item="1" data-small="1" data-middle="1" data-big="1" data-style="border-white">
@@ -13,54 +18,47 @@
         </div>
     </div>
 </div>
- 
- <div class="mainL" style="width: 1200px;margin-left: auto">
-        <div class="eventVoice clearfix">
-                        <div class="events fl clearfix">
-                            <div class="events_title"><b>{$sorts['100028']['name']}</b><span>{$sorts['100028']['ename']}</span><a class="more fr" href="{$sorts['100028']['url']}" target="_blank">更多</a>
-                            </div>
-                    <div class="fl">
 
-{news:{table=(news) field=(id,title,color,addtime,method,keywords,description) column=(100028) where=(ispass='1') limit=(8)}}     
 
-{if $news_i<=4}                      
-      <dl class="eventsDl eventsDlA">
-            <dt><a href="[news:url]" title="[news:title]" target="_blank" style="color:[news:color]">[news:title $len=40]</a></dt>
-            <dd>{date($news['addtime'],Y-m-d)}</dd>
-        </dl> 
-{/if}
 
- {if $news_i==4} 
-       </div> 
-      <div class="fr">
- {/if}
 
- {if $news_i>4} 
- <dl class="eventsDl eventsDlB">
-            <dt><a href="[news:url]" title="[news:title]" target="_blank" style="color:[news:color]">[news:title $len=40]</a></dt>
-            <dd>{date($news['addtime'],Y-m-d)}</dd>
-        </dl>
- {/if}  
+
+<div class="mainL" style="width: 1200px;margin-left: auto">
+         <div class="news clearfix">
+                        <div class="news_title"><b>{$sorts['100059']['name']}</b><span>News</span><a class="more fr" style="margin-right: 210px" href="{$sorts['100059']['url']}" target="_blank">更多</a></div>                    
+                      <div class="newsL fl clearfix">
+{news:{table=(news) field=(id,title,color,picture,addtime,method,description) column=(100059) where=(ispass='1') limit=(2)}}   
+   <div {if $news_i==1} class="newsLdl fl" {/if} {if $news_i==2} class="newsLdl fr" {/if} >
+<a href="[news:url]" title="[news:title]" target="_blank">
+<dl>
+<dt style="height:150px;background:url(dfiles/11107/img/mrt.jpg)"><img src="[news:picturepath]" style="height:150px;width:260px"> </dt>
+<dd>
+<h1>[news:title $len=20]</h1>
+<h2>{date($news['addtime'],m-d)}
+</h2>
+<p>[news:description]... </p>
+</dd>
+</dl>
+</a>  
+</div> 
 {/news}  
- </div>   
-  </div>
 
+</div> 
+
+<script>_showDynClickBatch(['dynclicks_u7_3217','dynclicks_u7_3205','dynclicks_u7_3192','dynclicks_u7_3189','dynclicks_u7_3172','dynclicks_u7_3127','dynclicks_u7_3089','dynclicks_u7_3094','dynclicks_u7_3076'],[3217,3205,3192,3189,3172,3127,3089,3094,3076],"wbnews", 1405884961)</script>
  
- <div class="voice fl clearfix" style="margin-left: 60px;"> 
-      <div class="voice_title"><b>{$sorts['100029']['name']}</b><span>{$sorts['100029']['ename']}</span><a class="more fr" href="{$sorts['100029']['url']}" target="_blank">更多</a>
-      </div>
+   <div class="newsR fl" style="margin-left: 80px;">
+   <ul>
+{news:{table=(news) field=(id,title,color,addtime,method,keywords,description) column=(100028) where=(ispass='1') limit=(8)}}     
+   <li><a href="[news:url]" title="[news:title]" target="_blank"> <span>{date($news['addtime'],m-d)}</span>[news:title $len=20]...</a></li>
+{/news}  
 
-        {news:{table=(news) field=(id,title,color,addtime,method,keywords,description) column=(100029) where=(ispass='1') limit=(7)}}
-
-        <dl class="clearfix" style="margin-top: 0">
-         <dt>{date($news['addtime'],m-d)}</dt>
-               <dd><p><a href="[news:url]" title="[news:title]" target="_blank" style="color:[news:color]"> [news:title $len=40]</a></p></dd>
-        </dl>
-        {/news}
-     </div>
-      </div>
+</ul>
+</div><script>_showDynClickBatch(['dynclicks_u8_3217','dynclicks_u8_3205','dynclicks_u8_3192','dynclicks_u8_3214','dynclicks_u8_3211','dynclicks_u8_3210','dynclicks_u8_3202','dynclicks_u8_3198','dynclicks_u8_3195','dynclicks_u8_3208'],[3217,3205,3192,3214,3211,3210,3202,3198,3195,3208],"wbnews", 1405884961)</script>
+     </div>     
 </div>
 
+ 
 
 <table   border="0" align="center" cellpadding="0" cellspacing="0" style="width: 1200px;margin-left: auto;">
     <tr>
@@ -94,13 +92,14 @@
                 </div>
             </div>
         </td>
+
         <td width="280" valign="top" bgcolor="#FFFFFF">
             <div class="yuevents fl clearfix">
-                <div class="events_title"><b>{$sorts['100032']['name']}</b>
-                    <span>Events</span><a class="more fr" href="{$sorts['100032']['url']}" target="_blank">更多</a>
+                <div class="events_title"><b>{$sorts['100029']['name']}</b>
+                    <span>Events</span><a class="more fr" href="{$sorts['100029']['url']}" target="_blank">更多</a>
                 </div>
                 <div>
-                    {news:{table=(news) field=(id,title,color,addtime,method,keywords,description) column=(100032) where=(ispass='1') limit=(4)}}
+                    {news:{table=(news) field=(id,title,color,addtime,method,keywords,description) column=(100029) where=(ispass='1') limit=(4)}}
                     <dl class="eventsDl eventsDlA">
                         <dt><a href="[news:url]" title="[news:title]" target="_blank">[news:title $len=40]</a></dt>
                         <dd>{date($news['addtime'],Y-m-d)}</dd>
